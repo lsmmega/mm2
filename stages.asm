14690cdcf036115e40802c42030ae62d772f4bef
lsmmega <pogmegalsm@outlook.com>
2025-01-15 23:30:57 +0800

    create air palettes

--- stages.asm
+++ stages.asm
@@ -3,3 +3,6 @@
 .SEGMENT "BANK00"
 .INCLUDE "stages/palettes/heat.asm"
 .INCLUDE "stages/palettes/wily1.asm"
+
+.SEGMENT "BANK01"
+.INCLUDE "stages/palettes/air.asm"
---
+++ stages/palettes/air.asm
@@ -0,0 +1,40 @@
+;frames setting.
+	.BYTE 4  ;frames total usage.
+	.BYTE 16 ;anime frames.
+
+;soild background palettes. 
+	.BYTE white_azure, white_azure, pale_azure,  pale
+	.BYTE black,       pale,        light_rose,  black
+	.BYTE black,       white_azure, white_azure, white_azure
+	.BYTE black,       white_azure, white_azure, white_azure
+
+;soild object palettes.
+	.BYTE white_azure, black, white_cyan,   light_azure
+	.BYTE black,       black, white,        pale_yellow
+	.BYTE black,       black, white_orange, pale
+	.BYTE black,       black, light_rose,   white_orange
+
+;anime background palettes.
+;1
+	.BYTE white_azure, white_azure, pale_azure,  pale
+	.BYTE black,       pale,        light_rose,  black
+	.BYTE black,       white_azure, white_azure, white_azure
+	.BYTE black,       white_azure, white_azure, white_azure
+
+;2
+	.BYTE white_azure, pale_azure,  pale,        pale
+	.BYTE black,       pale,        light_rose,  black
+	.BYTE black,       white_azure, white_azure, white_azure
+	.BYTE black,       white_azure, white_azure, white_azure
+
+;3
+	.BYTE white_azure, pale,        pale,        pale
+	.BYTE black,       pale,        light_rose,  black
+	.BYTE black,       white_azure, white_azure, white_azure
+	.BYTE black,       white_azure, white_azure, white_azure
+
+;4
+	.BYTE white_azure, pale_azure,  pale,        pale
+	.BYTE black,       pale,        light_rose,  black
+	.BYTE black,       white_azure, white_azure, white_azure
+	.BYTE black,       white_azure, white_azure, white_azure
