cf6d6e540429e86d604b0c4d7f2797cfd90ada10
lsmmega <pogmegalsm@outlook.com>
2025-01-15 23:42:58 +0800

    create crash palettes

---
+++ stages/palettes/crash.asm
@@ -0,0 +1,40 @@
+;frames setting.
+	.BYTE 0 ;frames total usage.
+	.BYTE 0 ;anime frames.
+
+;soild background palettes. 
+	.BYTE black, pale_chartreuse, light_yellow, light_blue
+	.BYTE black, light_blue,      white_orange, white
+	.BYTE black, pale_chartreuse, light_yellow, light_blue
+	.BYTE black, pale_chartreuse, light_yellow, dark_azure
+
+;soild object palettes.
+	.BYTE black, black, white_cyan,  light_azure
+	.BYTE black, black, white,       pale_yellow
+	.BYTE black, black, light_azure, pale
+	.BYTE black, black, light_azure, pale
+
+;anime background palettes. (unused)
+;1
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+
+;2
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+
+;3
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+
+;4
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
+	.BYTE black, black, black, black
