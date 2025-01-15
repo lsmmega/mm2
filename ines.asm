076c54d67e119e22e7bb194cef1d1c2787f2f26f
lsmmega <pogmegalsm@outlook.com>
2025-01-15 20:21:09 +0800

    quote

--- ines.asm
+++ ines.asm
@@ -11,5 +11,5 @@
 	system    = normal
 	.BYTE prg_size >> 4
 	.BYTE chr_size >> 3
-	.BYTE mapper &%00001111 << 4 | mirroring | battery << 1 | trainer << 2
+	.BYTE (mapper &%00001111) << 4 | mirroring | battery << 1 | trainer << 2
 	.BYTE mapper &%11110000 | system
