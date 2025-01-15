a3d6bfe44549f3ea577a5aab91aec9da6ef260db
lsmmega <pogmegalsm@outlook.com>
2025-01-15 20:15:04 +0800

    create track constants

---
+++ constants/track.asm
@@ -0,0 +1,69 @@
+.ENUM
+	track_flash
+	track_wood
+	track_crash
+	track_heat
+	track_air
+	track_metal
+	track_quick
+	track_bubble
+	track_wily1
+	track_wily3
+	track_stage_start
+	track_boss
+	track_stage_select
+	track_title
+	track_opening
+	track_game_over
+	track_pass_word
+	track_wily_castle
+	track_ufo
+	track_ending
+	track_credit
+	track_boss_defeated
+	track_wily_defeated
+	track_get_weapon
+	track_18                ;repeated game over
+	track_19                ;repeated game over
+	track_1a                ;repeated game over
+	track_1b                ;repeated game over
+	track_1c                ;repeated game over
+	track_1d                ;repeated game over
+	track_1e                ;repeated game over
+	track_1f                ;repeated game over
+	track_20                ;repeated game over
+	track_flash
+	track_22                ;unused
+	track_metal_blade
+	track_mm_shoot
+	track_enemy_shoot
+	track_damage_mm
+	track_force_beam
+	track_bar
+	track_landing
+	track_alien_destroyed
+	track_damage_enemy
+	track_dragon_appeared
+	track_recoil
+	track_crash_bomb_caught
+	track_change_weapon
+	track_teleport_in
+	track_leaf_shield
+	track_open_menu
+	track_33                ;unused
+	track_boss_door
+	track_atom_fire_1
+	track_atom_fire_2
+	track_atom_fire_3
+	track_atom_fire_release
+	track_fly_boy_landing
+	track_teleport_out
+	track_water
+	track_yoku_block
+	track_red_liquid_1
+	track_red_liquid_2
+	track_air_shooter
+	track_40                ;unused
+	track_death
+	track_1up
+.ENDENUM
--- includes.asm
+++ includes.asm
@@ -1,3 +1,4 @@
 .INCLUDE "constants/boolean.asm"
 .INCLUDE "constants/ines.asm"
 .INCLUDE "constants/palettes.asm"
+.INCLUDE "constants/track.asm"
