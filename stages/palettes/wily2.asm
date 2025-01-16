ee39a4b98f0b6cdb6e685eca11407ae390ce0539
lsmmega <pogmegalsm@outlook.com>
2025-01-16 22:12:48 +0800

    Create wily2.asm

---
+++ stages/palettes/wily2.asm
@@ -0,0 +1,40 @@
+;frames setting.
+	.BYTE 3 ;frames total usage.
+	.BYTE 4 ;anime frames.
+
+;soild background palettes. 
+	.BYTE black, light_gray,   light_yellow, dark_yellow
+	.BYTE black, black,        dark_yellow,  black
+	.BYTE black, pale_magenta, light_rose,   dark_rose
+	.BYTE black, pale_azure,   pale_azure,   black
+
+;soild object palettes.
+	.BYTE black, black, white_cyan,   light_azure
+	.BYTE black, black, white,        pale_yellow
+	.BYTE black, black, white_orange, pale
+	.BYTE black, black, light_rose,   white_orange
+
+;anime background palettes.
+;1
+	.BYTE black, light_gray,   light_yellow, dark_yellow
+	.BYTE black, black,        dark_yellow,  black
+	.BYTE black, pale_magenta, light_rose,   dark_rose
+	.BYTE black, pale_azure,   pale_azure,   black
+
+;2
+	.BYTE black, light_gray,   light_yellow, dark_yellow
+	.BYTE black, black,        dark_yellow,  black
+	.BYTE black, pale_magenta, light_rose,   dark_rose
+	.BYTE black, black,        pale_azure,   pale_azure
+
+;3
+	.BYTE black, light_gray,   light_yellow, dark_yellow
+	.BYTE black, black,        dark_yellow,  black
+	.BYTE black, pale_magenta, light_rose,   dark_rose
+	.BYTE black, pale_azure,   black,        pale_azure
+
+;4
+	.BYTE black, light_gray,   light_yellow, dark_yellow
+	.BYTE black, black,        dark_yellow,  black
+	.BYTE black, pale_magenta, light_rose,   dark_rose
+	.BYTE black, pale_azure,   pale_azure,   black
