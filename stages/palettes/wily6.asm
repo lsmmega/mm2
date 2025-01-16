d0b887861ec00f96715e9d2c8454ba285f79a9db
lsmmega <pogmegalsm@outlook.com>
2025-01-16 22:41:14 +0800

    Create wily6.asm

---
+++ stages/palettes/wily6.asm
@@ -0,0 +1,40 @@
+;frames setting.
+	.BYTE 0 ;frames total usage.
+	.BYTE 0 ;anime frames.
+
+;soild background palettes. 
+	.BYTE black, white_orange, light_yellow, dark_green
+	.BYTE black, black,        dark_spring,  black
+	.BYTE black, white,        light_gray,   dark_gray
+	.BYTE black, dark_spring,  black,        dark_spring
+
+;solid object palettes.
+	.BYTE black, black, white_cyan,  light_azure
+	.BYTE black, black, white,       pale_yellow
+	.BYTE black, black, light_azure, pale
+	.BYTE black, black, light_rose,  white_yellow
+
+;anime background palettes. (unused)
+;1
+	.BYTE black, white_orange, light_yellow, dark_green
+	.BYTE black, black,        dark_spring,  black
+	.BYTE black, white,        light_gray,   dark_gray
+	.BYTE black, dark_spring,  black,        dark_spring
+
+;2
+	.BYTE black, white_orange, light_yellow, dark_green
+	.BYTE black, black,        dark_spring,  black
+	.BYTE black, white,        light_gray,   dark_gray
+	.BYTE black, black,        dark_spring,  dark_spring
+
+;3
+	.BYTE black, white_orange, light_yellow, dark_green
+	.BYTE black, black,        dark_spring,  black
+	.BYTE black, white,        light_gray,   dark_gray
+	.BYTE black, dark_spring,  dark_spring,  black
+
+;4
+	.BYTE black, white_orange, light_yellow, dark_green
+	.BYTE black, black,        dark_spring,  black
+	.BYTE black, white,        light_gray,   dark_gray
+	.BYTE black, dark_spring,  dark_spring,  black
