af6264275839f8f4b07be94934ef8b20b6815753
lsmmega <pogmegalsm@outlook.com>
2025-01-16 22:36:14 +0800

    Create wily5.asm

---
+++ stages/palettes/wily5.asm
@@ -0,0 +1,40 @@
+;frames setting.
+	.BYTE 4 ;frames total usage.
+	.BYTE 6 ;anime frames.
+
+;soild background palettes. 
+	.BYTE black, pale,             pale_blue,       white_blue
+	.BYTE black, pale_orange,      white_orange,    light_orange
+	.BYTE black, light_chartreuse, dark_chartreuse, white_azure
+	.BYTE black, dark_azure,       dark_azure,      dark_azure
+
+;solid object palettes.
+	.BYTE black, black, white_cyan, light_azure
+	.BYTE black, black, white,      pale_yellow
+	.BYTE black, black, black,      black
+	.BYTE black, black, black,      black
+
+;anime background palettes.
+;1
+	.BYTE black, pale,             pale_blue,       white_blue
+	.BYTE black, pale_orange,      white_orange,    light_orange
+	.BYTE black, light_chartreuse, dark_chartreuse, white_azure
+	.BYTE black, dark_azure,       dark_azure,      dark_azure
+
+;2
+	.BYTE black, pale,             pale_blue,       white_blue
+	.BYTE black, pale_orange,      white_orange,    light_orange
+	.BYTE black, light_chartreuse, dark_chartreuse, white_azure
+	.BYTE black, dark_azure,       white_azure,     dark_azure
+
+;3
+	.BYTE black, pale,             pale_blue,       white_blue
+	.BYTE black, pale_orange,      white_orange,    light_orange
+	.BYTE black, light_chartreuse, dark_chartreuse, white_azure
+	.BYTE black, dark_azure,       dark_azure,      white_azure
+
+;4
+	.BYTE black, pale,             pale_blue,       white_blue
+	.BYTE black, pale_orange,      white_orange,    light_orange
+	.BYTE black, light_chartreuse, dark_chartreuse, white_azure
+	.BYTE black, dark_azure,       white_azure,     white_azure
