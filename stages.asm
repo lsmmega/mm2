31d9af6dcefe9e8ca7c15ae6ee800495cfb20169
lsmmega <pogmegalsm@outlook.com>
2025-01-16 22:41:17 +0800

    Update stages.asm

--- stages.asm
+++ stages.asm
@@ -6,3 +6,26 @@
 
 .SEGMENT "BANK01"
 .INCLUDE "stages/palettes/air.asm"
+.INCLUDE "stages/palettes/wily2.asm"
+
+.SEGMENT "BANK02"
+.INCLUDE "stages/palettes/wood.asm"
+.INCLUDE "stages/palettes/wily3.asm"
+
+.SEGMENT "BANK03"
+.INCLUDE "stages/palettes/bubble.asm"
+.INCLUDE "stages/palettes/wily4.asm"
+
+.SEGMENT "BANK04"
+.INCLUDE "stages/palettes/quick.asm"
+.INCLUDE "stages/palettes/wily5.asm"
+
+.SEGMENT "BANK05"
+.INCLUDE "stages/palettes/flash.asm"
+.INCLUDE "stages/palettes/wily6.asm"
+
+.SEGMENT "BANK06"
+.INCLUDE "stages/palettes/metal.asm"
+
+.SEGMENT "BANK07"
+.INCLUDE "stages/palettes/crash.asm"
