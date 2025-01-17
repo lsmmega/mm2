ea3dadd8327733986c03dc28bbc77fd5eedb210b
lsmmega <pogmegalsm@outlook.com>
2025-01-17 21:48:08 +0800

    Create metal.asm

---
+++ stages/scrolling/metal.asm
@@ -0,0 +1,9 @@
+;number of room right scrolling | then scroll direction.
+;metal.
+	.BYTE 9 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 8 | right_scrolling
+	.BYTE 0 | right_scrolling
+	.BYTE 0 | no_scrolling
+	.BYTE 0 | no_scrolling
+	.BYTE 0 | no_scrolling
