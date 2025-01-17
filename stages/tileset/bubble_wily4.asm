3ab318f40e94a04c96aba7a81d7102351abbcd7b
lsmmega <pogmegalsm@outlook.com>
2025-01-17 23:11:11 +0800

    Create bubble_wily4.asm

---
+++ stages/tileset/bubble_wily4.asm
@@ -0,0 +1,22 @@
+;scrolling tileset index.
+	.BYTE 0 * $12
+	.BYTE 1 * $12
+	.BYTE 1 * $12
+	.BYTE 1 * $12
+	.BYTE 2 * $12
+	.BYTE 0 * $12
+	.BYTE 1 * $12
+	.BYTE 3 * $12
+	.BYTE 0 * $12
+	.BYTE 4 * $12
+	.BYTE 4 * $12
+	.BYTE 1 * $12
+	.BYTE 2 * $12
+	.BYTE 4 * $12
+	.BYTE 1 * $12
+	.BYTE 5 * $12
+	.BYTE 5 * $12
+	.BYTE 5 * $12
+	.BYTE 5 * $12
+	.BYTE 6 * $12
+	.BYTE 7 * $12
