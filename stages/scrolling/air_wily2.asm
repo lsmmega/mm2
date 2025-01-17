d2b8382323f6c673fe5c3444671f05609f0d6d8d
lsmmega <pogmegalsm@outlook.com>
2025-01-17 21:35:54 +0800

    Create air_wily2.asm

---
+++ stages/scrolling/air_wily2.asm
@@ -0,0 +1,19 @@
+;number of room right scrolling | then scroll direction.
+;air.
+	.BYTE 9 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 8 | right_scrolling
+	.BYTE 0 | right_scrolling
+	.BYTE 0 | no_scrolling
+
+;wily2.
+	.BYTE 5 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 5 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 0 | down_scrolling
+	.BYTE 0 | no_scrolling
+	.BYTE 0 | no_scrolling
+	.BYTE 0 | no_scrolling
