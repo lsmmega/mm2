d333dc73c1b86beae2a093f52a94263d2110b2af
lsmmega <pogmegalsm@outlook.com>
2025-01-17 23:07:38 +0800

    Create heat_wily1.asm

---
+++ stages/tileset/heat_wily1.asm
@@ -0,0 +1,16 @@
+;scrolling tileset index.
+	.BYTE 0 * $12
+	.BYTE 2 * $12
+	.BYTE 2 * $12
+	.BYTE 1 * $12
+	.BYTE 0 * $12
+	.BYTE 0 * $12
+	.BYTE 3 * $12
+	.BYTE 5 * $12
+	.BYTE 1 * $12
+	.BYTE 1 * $12
+	.BYTE 1 * $12
+	.BYTE 0 * $12
+	.BYTE 0 * $12
+	.BYTE 4 * $12
+	.BYTE 1 * $12
