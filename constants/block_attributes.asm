550868703c2c878463c852176e85014a1d428ac3
lsmmega <pogmegalsm@outlook.com>
2025-01-18 17:36:15 +0800

    Create block_attributes.asm

---
+++ constants/block_attributes.asm
@@ -0,0 +1,17 @@
+;block attribute set.
+.ENUM
+	block_null
+	block_wall
+	block_ladder
+	block_spike
+	block_water
+	block_conveyor_belt_right
+	block_conveyor_belt_left
+	block_ice
+.ENDENUM
+
+;block attribute group set.
+	block_attribute_null = 0 * $40
+	block_attribute_wall = 1 * $40
+	block_attribute_2    = 2 * $40
+	block_attribute_3    = 3 * $40
