336aeae59bb761c16f041f45c2223bcae51bb6d2
lsmmega <pogmegalsm@outlook.com>
2025-10-11 10:35:15 +0800

    sfx 1up done

---
+++ audio/sfx/1up.asm
@@ -0,0 +1,87 @@
+sfx_1up:
+	sfx_priority 15
+	.BYTE sfx_pulse_2
+
+;1
+;sfx_pulse_2
+	sfx_frames 3
+	duty_cycle 3
+	volume 15
+	sfx_note nDb5
+
+;2
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nEb5
+
+;3
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nF_5
+
+;4
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nGb5
+
+;5
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nAb5
+
+;6
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nBb5
+
+;7
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nC_6
+
+;8
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nDb6
+
+;9
+;sfx_pulse_2
+	sfx_frames 3
+	volume 8
+	sfx_note nDb5
+
+;10
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nEb5
+
+;11
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nF_5
+
+;12
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nGb5
+
+;13
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nAb5
+
+;14
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nBb5
+
+;15
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nC_6
+
+;16
+;sfx_pulse_2
+	sfx_frames 3
+	sfx_note nDb6
+	sfx_end
