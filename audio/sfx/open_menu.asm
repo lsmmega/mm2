1ded88004257c17acf277903c83e4cf76110925e
lsmmega <pogmegalsm@outlook.com>
2025-10-11 10:37:18 +0800

    sfx open menu done

---
+++ audio/sfx/open_menu.asm
@@ -0,0 +1,26 @@
+sfx_open_menu:
+	sfx_priority 14
+	.BYTE sfx_pulse_2
+
+;1
+;sfx_pulse_2
+	sfx_frames 5
+	volume 15
+	duty_cycle 2
+	sfx_note nA_3
+
+;2
+;sfx_pulse_2
+	sfx_frames 5
+	sfx_note nE_3
+
+;3
+;sfx_pulse_2
+	sfx_frames 5
+	sfx_note nDb3
+
+;4
+;sfx_pulse_2
+	sfx_frames 5
+	sfx_note nA_4
+	sfx_end
