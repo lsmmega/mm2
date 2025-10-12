3bd96762d2a14f71b8202de060b7ea9f8803b6a5
lsmmega <pogmegalsm@outlook.com>
2025-10-13 00:23:42 +0800

    Create weapons.asm

---
+++ constants/weapons.asm
@@ -0,0 +1,14 @@
+;got weapon flags.
+	got_atomic_fire     = 1 << 0
+	got_air_shooter     = 1 << 1
+	got_leaf_shield     = 1 << 2
+	got_bubble_lead     = 1 << 3
+	got_quick_boomerang = 1 << 4
+	got_time_stopper    = 1 << 5
+	got_metal_blade     = 1 << 6
+	got_crash_bomber    = 1 << 7
+
+;got item flags.
+	got_item_1 = 1 << 0
+	got_item_2 = 1 << 1
+	got_item_3 = 1 << 2
