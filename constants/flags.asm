5e347fb8c08b84085d42e13a5c5e2e4bc4658a19
lsmmega <pogmegalsm@outlook.com>
2025-10-13 00:32:48 +0800

    Update flags.asm

--- constants/flags.asm
+++ constants/flags.asm
@@ -12,3 +12,13 @@
 	got_item_1 = 1 << 0
 	got_item_2 = 1 << 1
 	got_item_3 = 1 << 2
+
+;stage clear flags.
+	stage_heatman_claer   = 1 << 0
+	stage_airman_claer    = 1 << 1
+	stage_woodman_claer   = 1 << 2
+	stage_bubbleman_claer = 1 << 3
+	stage_quickman_claer  = 1 << 4
+	stage_flashman_claer  = 1 << 5
+	stage_metalman_claer  = 1 << 6
+	stage_crashman_claer  = 1 << 7
