7b4e2214be9ec667be82c07aae4245055cb3bb45
lsmmega <pogmegalsm@outlook.com>
2025-10-13 12:03:22 +0800

    Update flags.asm

--- constants/flags.asm
+++ constants/flags.asm
@@ -1,3 +1,9 @@
+;got item flags.
+	no_item    = 0
+	got_item_1 = 1 << 0
+	got_item_2 = 1 << 1
+	got_item_3 = 1 << 2
+
 ;got weapon flags.
 	got_atomic_fire     = 1 << 0
 	got_air_shooter     = 1 << 1
@@ -8,10 +14,15 @@
 	got_metal_blade     = 1 << 6
 	got_crash_bomber    = 1 << 7
 
-;got item flags.
-	got_item_1 = 1 << 0
-	got_item_2 = 1 << 1
-	got_item_3 = 1 << 2
+;rematch flags.
+	rematch_heatman_done   = 1 << 0
+	rematch_airman_done    = 1 << 1
+	rematch_woodman_done   = 1 << 2
+	rematch_bubbleman_done = 1 << 3
+	rematch_quickman_done  = 1 << 4
+	rematch_flashman_done  = 1 << 5
+	rematch_metalman_done  = 1 << 6
+	rematch_crashman_done  = 1 << 7
 
 ;stage clear flags.
 	stage_heatman_clear   = 1 << 0
