5e24c29bf57e524b900cb6a9b7351c829d248396
lsmmega <pogmegalsm@outlook.com>
2026-08-25 02:26:22 +0800

    add stage select clear ppu address

---
+++ data/stage_select/stage_select_clear_ppu_address.asm
@@ -0,0 +1,19 @@
+stage_select_clear_hi_ppu_address:
+	.HIBYTES $2186 ;heatman
+	.HIBYTES $208E ;airman
+	.HIBYTES $2196 ;woodman
+	.HIBYTES $2086 ;bubbleman
+	.HIBYTES $2096 ;quickman
+	.HIBYTES $228E ;flashman
+	.HIBYTES $2286 ;metalman
+	.HIBYTES $2296 ;crashman
+
+stage_select_clear_lo_ppu_address:
+	.LOBYTES $2186 ;heatman
+	.LOBYTES $208E ;airman
+	.LOBYTES $2196 ;woodman
+	.LOBYTES $2086 ;bubbleman
+	.LOBYTES $2096 ;quickman
+	.LOBYTES $228E ;flashman
+	.LOBYTES $2286 ;metalman
+	.LOBYTES $2296 ;crashman
