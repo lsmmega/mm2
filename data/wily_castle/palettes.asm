80d99ca2c1c3d58c45b010f3c1229fa01222fb38
lsmmega <pogmegalsm@outlook.com>
2026-08-31 00:56:16 +0800

    Create palettes.asm

---
+++ data/wily_castle/palettes.asm
@@ -0,0 +1,25 @@
+wily_castle_palettes:
+;background
+	.BYTE black, white_gray,   white_azure, light_azure
+	.BYTE black, white_gray,   light_gray,  dark_gray
+	.BYTE black, white_gray,   white_red,   light_rose
+	.BYTE black, light_orange, white_azure, dark_orange
+
+;objects
+	.BYTE black, light_red, white_chartreuse, dark_chartreuse
+	.BYTE black, black,     pale_gray,        pale_yellow
+	.BYTE black, black,     pale_gray,        white_yellow
+	.BYTE black, black,     light_blue,       white_cyan
+
+wily_castle_routing_palettes:
+;background
+	.BYTE black, light_azure,  light_azure, light_azure
+	.BYTE black, light_azure,  light_azure, light_azure
+	.BYTE black, light_azure,  light_azure, light_azure
+	.BYTE black, light_orange, light_azure, dark_orange
+
+;objects
+	.BYTE black, light_red, white_chartreuse, dark_chartreuse
+	.BYTE black, black,     pale_gray,        pale_yellow
+	.BYTE black, black,     white_yellow,     pale_gray
+	.BYTE black, black,     light_blue,       white_cyan
