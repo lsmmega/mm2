c1ce3b9158799334cabdcc3941b10fb7feef98e4
lsmmega <pogmegalsm@outlook.com>
2026-08-31 00:56:13 +0800

    Create upper_screen_set.asm

---
+++ data/wily_castle/upper_screen_set.asm
@@ -0,0 +1,6 @@
+wily_castle_upper_screen_set:
+	.BYTE $E8, $E8, $E8, $E8, $E8, $E8, $E8, $E8
+	.BYTE $E8, $E8, $E8, $E8, $E8, $E8, $E8
+
+;attributes
+	.BYTE topleft_palette_0 | topright_palette_0 | bottomleft_palette_0 | bottomright_palette_0
