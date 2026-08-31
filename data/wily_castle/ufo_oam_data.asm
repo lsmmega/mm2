85a07f2e6f725c2acd8d98eccaa4b9892456300e
lsmmega <pogmegalsm@outlook.com>
2026-08-31 21:37:01 +0800

    add ufo oam movement speed

---
+++ data/wily_castle/ufo_oam_data.asm
@@ -0,0 +1,10 @@
+wily_castle_ufo_oam_index_pointer:
+	.BYTE $00, $00, $07, $08, $09
+
+wily_castle_ufo_xcoord_speed_fraction:
+;right, left
+	.BYTE $18, -$17
+
+wily_castle_ufo_xcoord_speed:
+;right, left
+	.BYTE +0, -1
--- data/wily_castle/ufo_oam_index.asm
+++
@@ -1,2 +0,0 @@
-wily_castle_ufo_oam_index_pointer:
-	.BYTE $00, $00, $07, $08, $09
