684cd08dcfc0956ab9d327fead6f89100bc099fe
lsmmega <pogmegalsm@outlook.com>
2026-09-03 00:15:48 +0800

    add intro and title palettes

--- 13.asm
+++ 13.asm
@@ -79,6 +79,7 @@ _intro:
 .INCLUDE "engine/draw_intro_oam.asm"
 .INCLUDE "engine/title_megaman_oam.asm"
 .INCBIN  "13/13_22_1_0.bin"
+.INCLUDE "data/intro/palettes.asm"
 .INCLUDE "data/intro/rockman_title_oam_coord.asm" ;unused
 .INCLUDE "data/intro/intro_oam.asm"
 .INCLUDE "data/intro/oam_sprites.asm"
---
+++ data/intro/palettes.asm
@@ -0,0 +1,54 @@
+intro_fade_out_palettes:
+;1
+;background
+	.BYTE black, black, black, dark_magenta
+	.BYTE black, black, black, black
+	.BYTE black, black, black, dark_orange
+	.BYTE black, black, black, dark_gray
+
+;objects
+	.BYTE black, black, white_cyan, light_azure
+	.BYTE black, black, pale_gray,  pale_yellow
+	.BYTE black, black, dark_cyan,  dark_gray
+	.BYTE black, black, black,      dark_gray
+
+;2
+;background
+	.BYTE black, dark_gray, dark_violet,  light_magenta
+	.BYTE black, black,     dark_azure,   dark_gray
+	.BYTE black, dark_gray, dark_magenta, light_orange
+	.BYTE black, dark_gray, dark_gray,    light_gray
+
+;objects
+	.BYTE black, black,      white_cyan, light_azure
+	.BYTE black, black,      pale_gray,  pale_yellow
+	.BYTE black, light_gray, light_cyan, dark_azure
+	.BYTE black, dark_gray,  dark_gray,  light_gray
+
+intro_fade_out_final_palettes:
+;3
+;background
+	.BYTE black, dark_violet,  light_violet,  white_magenta
+	.BYTE black, black,        light_azure,   dark_cyan
+	.BYTE black, dark_magenta, light_magenta, white_orange
+	.BYTE black, dark_gray,    light_gray,    pale_gray
+
+;objects
+	.BYTE black, black,     white_cyan, light_azure
+	.BYTE black, black,     pale_gray,  pale_yellow
+	.BYTE black, pale_gray, white_cyan, light_azure
+	.BYTE black, dark_gray, light_gray, pale_gray
+
+title_hidden_palettes:
+;background
+	.BYTE black, black,     black,      black
+	.BYTE black, black,     black,      black
+	.BYTE black, black,     black,      black
+	.BYTE black, dark_gray, light_gray, pale_gray
+
+title_fade_out_palettes:
+;background
+	.BYTE white_red, light_rose,  white_gray, dark_red
+	.BYTE black,     pale_gray,   white_cyan, light_rose
+	.BYTE white_red, white_azure, white_gray, dark_spring
+	.BYTE black,     dark_gray,   light_gray, pale_gray
