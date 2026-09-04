c1fb763293d995536d015d44c3c0cda5d77eb017
lsmmega <pogmegalsm@outlook.com>
2026-09-05 00:22:26 +0800

    Update password.asm

--- data/password/password.asm
+++ data/password/password.asm
@@ -1,10 +1,10 @@
-;password boss clear set
+password_boss_clear_set:
 	.BYTE no_boss_clear,        boss_heatman_clear,  no_boss_clear,        boss_quickman_clear, boss_woodman_clear
 	.BYTE boss_flashman_clear,  no_boss_clear,       boss_bubbleman_clear, boss_quickman_clear, boss_crashman_clear
 	.BYTE boss_bubbleman_clear, boss_airman_clear,   boss_woodman_clear,   no_boss_clear,       boss_heatman_clear
 	.BYTE boss_metalman_clear,  boss_crashman_clear, boss_airman_clear,    boss_flashman_clear, boss_metalman_clear
 
-;password boolean
+password_boolean:
 	.BYTE boss_clear_yes, boss_clear_yes, boss_clear_yes, boss_clear_yes, boss_clear_no
 	.BYTE boss_clear_yes, boss_clear_yes, boss_clear_no,  boss_clear_no,  boss_clear_yes
 	.BYTE boss_clear_yes, boss_clear_no,  boss_clear_yes, boss_clear_yes, boss_clear_no
